0a1d08c4674b4cbce196c5598e977c563feea8ef
yithemes <yithemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-07 07:31:37 +0000

    Version 1.0.4 - Fixed: next button missing
    git-svn-id: https://plugins.svn.wordpress.org/yith-woocommerce-multi-step-checkout/trunk@1214631 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class.yith-multistep-checkout-frontend.php
+++ includes/class.yith-multistep-checkout-frontend.php
@@ -73,7 +73,7 @@ if ( ! class_exists( 'YITH_Multistep_Checkout_Frontend' ) ) {
             $script = apply_filters( 'yith_wcms_main_script', 'multistep.js' );
             $script = function_exists( 'yit_load_js_file' ) ? yit_load_js_file( $script ) : str_replace( '.js', '.min.js', $script );
 
-            wp_register_script( 'yith-wcms-step', YITH_WCMS_ASSETS_URL . 'js/' . $script, array( 'wc-checkout' ), '1.0.0', true );
+            wp_register_script( 'yith-wcms-step', YITH_WCMS_ASSETS_URL . 'js/' . $script, array( 'wc-checkout' ), '1.0.1', true );
             wp_enqueue_script( 'yith-wcms-step' );
 
             do_action( 'yith_wcms_enqueue_scripts' );
